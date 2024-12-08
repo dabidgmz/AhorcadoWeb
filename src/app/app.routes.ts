3cf0b71e87c6000a7caaac40dad985d7a162ef84
David Herrera <david.gmzherrera28@gmail.com>
2024-12-08 00:56:25 -0600

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
+import { IndexUsersComponent } from './index-user/index-user.component';
+import { LoginFormComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CodeVerifyComponent } from './code-verify/code-verify.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginFormComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'users', component: IndexUsersComponent},
+    { path: 'code', component: CodeVerifyComponent},
+    {path:'login',component:LoginFormComponent},
+    { path: '', component: LoginFormComponent },
+    { path: '**', redirectTo: '' },
+];
+
+export class AppRoutingModule {}
